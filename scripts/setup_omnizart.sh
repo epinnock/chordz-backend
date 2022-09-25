4faf5b00dd27894ba5c4658ac1978158b8bddda7
epinnock <epinnock@gmail.com>
2022-09-25 13:24:51 +0000

    changed omnizart setupt script

--- scripts/setup_omnizart.sh
+++ scripts/setup_omnizart.sh
@@ -1,7 +1,7 @@
-#git clone https://github.com/Music-and-Culture-Technology-Lab/omnizart.git
-#cd omnizart
-pip install -U pip
 pip3 install --upgrade cython
-#omnizart download-checkpoints
-#cd ..
-#mkdir songs
+git clone https://github.com/Music-and-Culture-Technology-Lab/omnizart.git
+cd omnizart
+pip install -U pip
+omnizart download-checkpoints
+cd ..
+mkdir songs
