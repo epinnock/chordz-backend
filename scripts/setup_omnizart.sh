2f5748595c4ece800f52707f76966ab305305cf0
epinnock <epinnock@gmail.com>
2022-10-07 05:01:24 +0000

    removed omnizart script to speed up deployment

--- scripts/setup_omnizart.sh
+++ scripts/setup_omnizart.sh
@@ -1,9 +1,10 @@
-pip3 install --upgrade cython
-git clone https://github.com/Music-and-Culture-Technology-Lab/omnizart.git
-cd omnizart
-pip install -U pip
-pip install numpy
-pip install .
-omnizart download-checkpoints
-cd ..
-mkdir songs
+
+#pip3 install --upgrade cython
+#git clone https://github.com/Music-and-Culture-Technology-Lab/omnizart.git
+#cd omnizart
+#pip install -U pip
+#pip install numpy
+#pip install .
+#omnizart download-checkpoints
+#cd ..
+#mkdir songs
